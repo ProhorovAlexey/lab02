0a89576bcf86b9abde9e24799c8b0239a260c586
ProhorovAlexey <prokhorovaf@student.bmstu.ru>
2022-03-16 23:01:03 +0300

    second commit

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 include <iostream>
+include<string>
 
 using namespace std;
 
 int main(){
-cout<<"Hello, world";
+string name;
+cout<<"Enter username: ";
+cin>>name;
+cout<<"Hello, world from "<<name;
 return 0;
 }
